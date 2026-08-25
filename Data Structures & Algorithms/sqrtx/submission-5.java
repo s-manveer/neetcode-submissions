34a83de9f20cf0498463ecd9a66ca8489ef75a71
s-manveer <98184626+s-manveer@users.noreply.github.com>
2026-08-25 23:47:31 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        int ans = -1;
+
+        if (x == 0) return 0; 
+
+        if (x == 1) return 1;
+
+        while (low <= high) {
+            int mid = (low+high)/2;
+
+            if (mid == x/mid) {
+                return mid;
+            } else if (mid > x/mid) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+                ans = mid;
+            }
+        }
+
+        return ans;
+    }
+}

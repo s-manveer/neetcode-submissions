144a14e2e47ed5003e547133c40979f29597b02b
s-manveer <98184626+s-manveer@users.noreply.github.com>
2026-09-08 01:14:32 +0530

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+
+        while(i < n) {
+            int correctIndex = nums[i] - 1;
+            
+            if (nums[i] <= 0 || nums[i] > n || nums[correctIndex] == nums[i]) {
+                i++;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }
+        }
+
+        for(int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1) return j+1;
+        }
+
+        return n+1;
+    }
+}
